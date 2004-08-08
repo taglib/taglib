60959a1b5b9c82636da99feb1d3d5bcdf964abe3
Scott Wheeler <wheeler@kde.org>
2004-08-08 06:04:42 +0000

    add api docs
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/kdesupport/taglib@336790 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- mpeg/id3v2/frames/uniquefileidentifierframe.h
+++ mpeg/id3v2/frames/uniquefileidentifierframe.h
@@ -28,18 +28,59 @@ namespace TagLib {
 
   namespace ID3v2 {
 
+    /*!
+     * This is an implementation of ID3v2 unique file identifier frames.  This
+     * frame is used to identify the file in an arbitrary database identified
+     * by the owner field.
+     */
+
+    //! An implementation of ID3v2 unique identifier frames
+
     class UniqueFileIdentifierFrame : public ID3v2::Frame
     {
       friend class FrameFactory;
 
     public:
+      /*!
+       * Creates a uniqe file identifier frame based on \a data.
+       */
       UniqueFileIdentifierFrame(const ByteVector &data);
+
+      /*!
+       * Creates a unique file identifier frame with the owner \a owner and
+       * the identification \a id.
+       */
       UniqueFileIdentifierFrame(const String &owner, const ByteVector &id);
 
+      /*!
+       * Returns the owner for the frame; essentially this is the key for
+       * determining which identification scheme this key belongs to.  This
+       * will usually either be an email address or URL for the person or tool
+       * used to create the unique identifier.
+       *
+       * \see setOwner()
+       */
       String owner() const;
+
+      /*!
+       * Returns the unique identifier.  Though sometimes this is a text string
+       * it also may be binary data and as much should be assumed when handling
+       * it.
+       */
       ByteVector identifier() const;
 
+      /*!
+       * Sets the owner of the identification scheme to \a s.
+       *
+       * \see owner()
+       */
       void setOwner(const String &s);
+
+      /*!
+       * Sets the unique file identifier to \a v.
+       *
+       * \see identifier()
+       */
       void setIdentifier(const ByteVector &v);
 
       virtual String toString() const;
