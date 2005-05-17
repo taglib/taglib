6ceb7a8151c3320b6b3e8e01e884216cda64375c
Scott Wheeler <wheeler@kde.org>
2005-05-17 22:38:01 +0000

    Patch from Umesh Shankar to make sure that the frameID is current.
    CCMAIL:Umesh Shankar <ushankar@cs.berkeley.edu>
    
    
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/kdesupport/taglib@415233 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- mpeg/id3v2/id3v2framefactory.cpp
+++ mpeg/id3v2/id3v2framefactory.cpp
@@ -103,6 +103,10 @@ Frame *FrameFactory::createFrame(const ByteVector &data, uint version) const
     return 0;
   }
 
+  // updateFrame() might have updated the frame ID.
+
+  frameID = header->frameID();
+
   // This is where things get necissarily nasty.  Here we determine which
   // Frame subclass (or if none is found simply an Frame) based
   // on the frame ID.  Since there are a lot of possibilities, that means
