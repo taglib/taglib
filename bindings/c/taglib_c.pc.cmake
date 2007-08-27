7b0f4f839c95ab098fec8c19f302a472acdf90dd
Laurent Montel <montel@kde.org>
2007-08-27 07:04:18 +0000

    Don't hardcode version
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/kdesupport/taglib@705092 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- bindings/c/taglib_c.pc.cmake
+++ bindings/c/taglib_c.pc.cmake
@@ -7,6 +7,6 @@ includedir=${INCLUDE_INSTALL_DIR}
 Name: TagLib C Bindings
 Description: Audio meta-data library (C bindings)
 Requires: taglib
-Version: 1.4
+Version: ${TAGLIB_LIB_MAJOR_VERSION}.${TAGLIB_LIB_MINOR_VERSION}.${TAGLIB_LIB_PATCH_VERSION}
 Libs: -L${LIB_INSTALL_DIR} -ltag_c
 Cflags: -I=${INCLUDE_INSTALL_DIR}/taglib 
