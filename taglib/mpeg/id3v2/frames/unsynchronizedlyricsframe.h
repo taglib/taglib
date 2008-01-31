0769375072cf70bacbb35ea7bbe3c9dae9e662af
Scott Wheeler <wheeler@kde.org>
2008-01-31 23:08:02 +0000

    SVN_SILENT wrong license header
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/kdesupport/taglib@769265 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- taglib/mpeg/id3v2/frames/unsynchronizedlyricsframe.h
+++ taglib/mpeg/id3v2/frames/unsynchronizedlyricsframe.h
@@ -19,6 +19,10 @@
  *   License along with this library; if not, write to the Free Software   *
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
  *   USA                                                                   *
+ *                                                                         *
+ *   Alternatively, this file is available under the Mozilla Public        *
+ *   License Version 1.1.  You may obtain a copy of the License at         *
+ *   http://www.mozilla.org/MPL/                                           *
  ***************************************************************************/
 
 #ifndef TAGLIB_UNSYNCHRONIZEDLYRICSFRAME_H
