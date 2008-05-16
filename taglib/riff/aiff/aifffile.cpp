d367a2f429f8eec0e096df66108706f8b6a2e0d7
Scott Wheeler <wheeler@kde.org>
2008-05-16 04:20:50 +0000

    Add the skeleton of an AIFF implementation.
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/kdesupport/taglib@808225 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ taglib/riff/aiff/aifffile.cpp
@@ -0,0 +1,96 @@
+/***************************************************************************
+    copyright            : (C) 2006 by Lukáš Lalinský
+    email                : lalinsky@gmail.com
+
+    copyright            : (C) 2004 by Allan Sandfeld Jensen
+    email                : kde@carewolf.org
+                           (original MPC implementation)
+ ***************************************************************************/
+
+/***************************************************************************
+ *   This library is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License version   *
+ *   2.1 as published by the Free Software Foundation.                     *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful, but   *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+ *   USA                                                                   *
+ *                                                                         *
+ *   Alternatively, this file is available under the Mozilla Public        *
+ *   License Version 1.1.  You may obtain a copy of the License at         *
+ *   http://www.mozilla.org/MPL/                                           *
+ ***************************************************************************/
+
+#include <tbytevector.h>
+#include <tdebug.h>
+#include <id3v2tag.h>
+
+#include "aifffile.h"
+
+using namespace TagLib;
+
+class RIFF::AIFF::File::FilePrivate
+{
+public:
+  FilePrivate()
+  {
+
+  }
+
+  ~FilePrivate()
+  {
+
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// public members
+////////////////////////////////////////////////////////////////////////////////
+
+RIFF::AIFF::File::File(FileName file, bool readProperties,
+                       Properties::ReadStyle propertiesStyle) : RIFF::File(file, BigEndian)
+{
+  d = new FilePrivate;
+  if(isOpen())
+    read(readProperties, propertiesStyle);
+}
+
+RIFF::AIFF::File::~File()
+{
+  delete d;
+}
+
+ID3v2::Tag *RIFF::AIFF::File::tag() const
+{
+  return 0;
+}
+
+RIFF::AIFF::Properties *RIFF::AIFF::File::audioProperties() const
+{
+  return 0;
+}
+
+bool RIFF::AIFF::File::save()
+{
+  if(readOnly()) {
+    debug("RIFF::AIFF::File::save() -- File is read only.");
+    return false;
+  }
+
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// private members
+////////////////////////////////////////////////////////////////////////////////
+
+void RIFF::AIFF::File::read(bool readProperties, Properties::ReadStyle /* propertiesStyle */)
+{
+
+}
