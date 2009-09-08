9b17ca219cb41262cb32fb651bee8a7e352ff541
Lukáš Lalinský <lalinsky@gmail.com>
2009-09-08 08:52:57 +0000

    Add option for building a static version of taglib
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/kdesupport/taglib@1021124 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- taglib/taglib_export.h
+++ taglib/taglib_export.h
@@ -26,7 +26,7 @@
 #ifndef TAGLIB_EXPORT_H
 #define TAGLIB_EXPORT_H
 
-#if defined(_WIN32) || defined(_WIN64)
+#if !defined(TAGLIB_STATIC) && (defined(_WIN32) || defined(_WIN64))
 #ifdef MAKE_TAGLIB_LIB
 #define TAGLIB_EXPORT __declspec(dllexport)
 #else
