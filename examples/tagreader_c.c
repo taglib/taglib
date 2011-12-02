c0e5d3a7bb3b222c21c1be599df3557656cb26ee
David Eisner <david.eisner@oriel.oxon.org>
2011-12-02 14:03:44 +0000

    Album Artist and Grouping C bindings and example

--- examples/tagreader_c.c
+++ examples/tagreader_c.c
@@ -53,13 +53,15 @@ int main(int argc, char *argv[])
 
     if(tag != NULL) {
       printf("-- TAG --\n");
-      printf("title   - \"%s\"\n", taglib_tag_title(tag));
-      printf("artist  - \"%s\"\n", taglib_tag_artist(tag));
-      printf("album   - \"%s\"\n", taglib_tag_album(tag));
-      printf("year    - \"%i\"\n", taglib_tag_year(tag));
-      printf("comment - \"%s\"\n", taglib_tag_comment(tag));
-      printf("track   - \"%i\"\n", taglib_tag_track(tag));
-      printf("genre   - \"%s\"\n", taglib_tag_genre(tag));
+      printf("title          - \"%s\"\n", taglib_tag_title(tag));
+      printf("artist         - \"%s\"\n", taglib_tag_artist(tag));
+      printf("album artist   - \"%s\"\n", taglib_tag_album_artist(tag));
+      printf("album          - \"%s\"\n", taglib_tag_album(tag));
+      printf("year           - \"%i\"\n", taglib_tag_year(tag));
+      printf("comment        - \"%s\"\n", taglib_tag_comment(tag));
+      printf("track          - \"%i\"\n", taglib_tag_track(tag));
+      printf("genre          - \"%s\"\n", taglib_tag_genre(tag));
+      printf("grouping       - \"%s\"\n", taglib_tag_grouping(tag));
     }
 
     if(properties != NULL) {
