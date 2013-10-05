dcfb71bcb41a0ed2c52fbac7b30f5489e3556fdb
Lukáš Lalinský <lukas@oxygene.sk>
2013-10-05 10:53:15 +0200

    Update version number to 1.9

--- taglib/toolkit/taglib.h
+++ taglib/toolkit/taglib.h
@@ -29,7 +29,7 @@
 #include "taglib_config.h"
 
 #define TAGLIB_MAJOR_VERSION 1
-#define TAGLIB_MINOR_VERSION 8
+#define TAGLIB_MINOR_VERSION 9
 #define TAGLIB_PATCH_VERSION 0
 
 #if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 1))
