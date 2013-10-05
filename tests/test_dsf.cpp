e5c2232b10e0b34642ce11bd318f93335042c8b6
Stephen F. Booth <me@sbooth.org>
2013-10-05 14:03:19 -0400

    Added tag setting test

--- tests/test_dsf.cpp
+++ tests/test_dsf.cpp
@@ -13,6 +13,7 @@ class TestDSF : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE(TestDSF);
   CPPUNIT_TEST(testBasic);
+  CPPUNIT_TEST(testTags);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -24,6 +25,22 @@ public:
     CPPUNIT_ASSERT_EQUAL(2822400, props->sampleRate());
     CPPUNIT_ASSERT_EQUAL(1, props->channels());
     CPPUNIT_ASSERT_EQUAL(1, props->bitsPerSample());
+  }
+
+  void testTags()
+  {
+    ScopedFileCopy copy("empty", ".dsf");
+    string newname = copy.fileName();
+
+    DSF::File *f = new DSF::File(newname.c_str());
+    CPPUNIT_ASSERT_EQUAL(String(""), f->tag()->artist());
+    f->tag()->setArtist("The Artist");
+    f->save();
+    delete f;
+
+    f = new DSF::File(newname.c_str());
+    CPPUNIT_ASSERT_EQUAL(String("The Artist"), f->tag()->artist());
+    delete f;
   }
 
 };
