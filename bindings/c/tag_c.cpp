bd63903ac81b907428e310f7e4ccb231058a1588
Bart van der Velden <bart@muckingabout.eu>
2014-02-17 18:18:36 +0100

    Use 0 instead of FALSE to make g++ happy

--- bindings/c/tag_c.cpp
+++ bindings/c/tag_c.cpp
@@ -46,12 +46,12 @@ static bool stringManagementEnabled = true;
 
 void taglib_set_strings_unicode(BOOL unicode)
 {
-  unicodeStrings = bool(unicode != FALSE);
+  unicodeStrings = bool(unicode != 0);
 }
 
 void taglib_set_string_management_enabled(BOOL management)
 {
-  stringManagementEnabled = bool(management != FALSE);
+  stringManagementEnabled = bool(management != 0);
 }
 
 void taglib_free(void* pointer)
