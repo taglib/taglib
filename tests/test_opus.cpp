72cd379f30492102f1a3b5ba18614da5f432c399
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2014-03-10 09:27:04 +0900

    opus: Estimate the bitrate if possible

--- tests/test_opus.cpp
+++ tests/test_opus.cpp
@@ -23,7 +23,7 @@ public:
   {
     Ogg::Opus::File f(TEST_FILE_PATH_C("correctness_gain_silent_output.opus"));
     CPPUNIT_ASSERT_EQUAL(7, f.audioProperties()->length());
-    CPPUNIT_ASSERT_EQUAL(0, f.audioProperties()->bitrate());
+    CPPUNIT_ASSERT_EQUAL(41, f.audioProperties()->bitrate());
     CPPUNIT_ASSERT_EQUAL(1, f.audioProperties()->channels());
     CPPUNIT_ASSERT_EQUAL(48000, f.audioProperties()->sampleRate());
     CPPUNIT_ASSERT_EQUAL(48000, ((Ogg::Opus::Properties *)f.audioProperties())->inputSampleRate());
