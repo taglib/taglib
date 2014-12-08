fb1c744dafc13495240e8eaa6975654c1894c18b
Bart van der Velden <bart@muckingabout.eu>
2014-12-08 09:42:04 -0800

    Compile without warnings with MSVC

--- bindings/c/tag_c.cpp
+++ bindings/c/tag_c.cpp
@@ -46,12 +46,12 @@ static bool stringManagementEnabled = true;
 
 void taglib_set_strings_unicode(BOOL unicode)
 {
-  unicodeStrings = bool(unicode);
+  unicodeStrings = (unicode != 0);
 }
 
 void taglib_set_string_management_enabled(BOOL management)
 {
-  stringManagementEnabled = bool(management);
+  stringManagementEnabled = (management != 0);
 }
 
 void taglib_free(void* pointer)
