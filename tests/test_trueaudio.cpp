7a9f503cdb729c8b2e6df95d5a752c29dbd1912f
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-06-17 00:08:41 +0900

    Add some tests to check if the audio stream is intact after save.

--- tests/test_trueaudio.cpp
+++ tests/test_trueaudio.cpp
@@ -15,7 +15,10 @@ class TestTrueAudio : public CppUnit::TestFixture
   CPPUNIT_TEST(testReadPropertiesWithoutID3v2);
   CPPUNIT_TEST(testSaveID3v1Twice);
   CPPUNIT_TEST(testSaveID3v2Twice);
-  CPPUNIT_TEST(testSaveTagCombination);
+  CPPUNIT_TEST(testSaveTags1);
+  CPPUNIT_TEST(testSaveTags2);
+  CPPUNIT_TEST(testStripTags1);
+  CPPUNIT_TEST(testStripTags2);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -32,15 +35,22 @@ public:
     ScopedFileCopy copy1("empty", ".tta");
     ScopedFileCopy copy2("empty", ".tta");
 
+    ByteVector audioStream;
     {
       TrueAudio::File f(copy1.fileName().c_str());
       CPPUNIT_ASSERT(!f.hasID3v1Tag());
       CPPUNIT_ASSERT_EQUAL((long)79538, f.length());
 
+      f.seek(0x0000);
+      audioStream = f.readBlock(79538);
+
       f.ID3v1Tag(true)->setTitle("01234 56789 ABCDE FGHIJ");
       f.save();
       CPPUNIT_ASSERT(f.hasID3v1Tag());
       CPPUNIT_ASSERT_EQUAL((long)79666, f.length());
+
+      f.seek(0x0000);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
     }
 
     {
@@ -50,6 +60,15 @@ public:
       f.save();
       CPPUNIT_ASSERT(f.hasID3v1Tag());
       CPPUNIT_ASSERT_EQUAL((long)79666, f.length());
+
+      f.seek(0x0000);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
+
+      f.ID3v1Tag(true)->setTitle("");
+      f.save();
+
+      f.seek(0x0000);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
     }
   }
 
@@ -58,15 +77,22 @@ public:
     ScopedFileCopy copy1("empty", ".tta");
     ScopedFileCopy copy2("empty", ".tta");
 
+    ByteVector audioStream;
     {
       TrueAudio::File f(copy1.fileName().c_str());
       CPPUNIT_ASSERT(!f.hasID3v2Tag());
       CPPUNIT_ASSERT_EQUAL((long)79538, f.length());
 
+      f.seek(0x0000);
+      audioStream = f.readBlock(79538);
+
       f.ID3v2Tag(true)->setTitle("01234 56789 ABCDE FGHIJ");
       f.save();
       CPPUNIT_ASSERT(f.hasID3v2Tag());
       CPPUNIT_ASSERT_EQUAL((long)80606, f.length());
+
+      f.seek(0x042C);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
     }
 
     {
@@ -76,15 +102,28 @@ public:
       f.save();
       CPPUNIT_ASSERT(f.hasID3v2Tag());
       CPPUNIT_ASSERT_EQUAL((long)80606, f.length());
+
+      f.seek(0x042C);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
+
+      f.ID3v2Tag(true)->setTitle("");
+      f.save();
+
+      f.seek(0x0000);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
     }
   }
 
-  void testSaveTagCombination()
+  void testSaveTags1()
   {
-    ScopedFileCopy copy1("empty", ".tta");
+    ScopedFileCopy copy("empty", ".tta");
 
+    ByteVector audioStream;
     {
-      TrueAudio::File f(copy1.fileName().c_str());
+      TrueAudio::File f(copy.fileName().c_str());
+      f.seek(0x0000);
+      audioStream = f.readBlock(79538);
+
       CPPUNIT_ASSERT(!f.hasID3v1Tag());
       CPPUNIT_ASSERT(!f.hasID3v2Tag());
       f.ID3v1Tag(true)->setTitle("01234 56789 ABCDE FGHIJ");
@@ -100,19 +139,29 @@ public:
     }
 
     {
-      TrueAudio::File f(copy1.fileName().c_str());
+      TrueAudio::File f(copy.fileName().c_str());
       CPPUNIT_ASSERT_EQUAL((long)80734, f.length());
       CPPUNIT_ASSERT(f.hasID3v1Tag());
       CPPUNIT_ASSERT(f.hasID3v2Tag());
 
       CPPUNIT_ASSERT_EQUAL(String("01234 56789 ABCDE FGHIJ"), f.ID3v1Tag()->title());
       CPPUNIT_ASSERT_EQUAL(String("01234 56789 ABCDE FGHIJ"), f.ID3v2Tag()->title());
+
+      f.seek(0x042C);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
     }
+  }
 
-    ScopedFileCopy copy2("empty", ".tta");
+  void testSaveTags2()
+  {
+    ScopedFileCopy copy("empty", ".tta");
 
+    ByteVector audioStream;
     {
-      TrueAudio::File f(copy2.fileName().c_str());
+      TrueAudio::File f(copy.fileName().c_str());
+      f.seek(0x0000);
+      audioStream = f.readBlock(79538);
+
       CPPUNIT_ASSERT(!f.hasID3v1Tag());
       CPPUNIT_ASSERT(!f.hasID3v2Tag());
       f.ID3v2Tag(true)->setTitle("01234 56789 ABCDE FGHIJ");
@@ -128,13 +177,120 @@ public:
     }
 
     {
-      TrueAudio::File f(copy2.fileName().c_str());
+      TrueAudio::File f(copy.fileName().c_str());
       CPPUNIT_ASSERT_EQUAL((long)80734, f.length());
       CPPUNIT_ASSERT(f.hasID3v1Tag());
       CPPUNIT_ASSERT(f.hasID3v2Tag());
 
       CPPUNIT_ASSERT_EQUAL(String("01234 56789 ABCDE FGHIJ"), f.ID3v1Tag()->title());
       CPPUNIT_ASSERT_EQUAL(String("01234 56789 ABCDE FGHIJ"), f.ID3v2Tag()->title());
+
+      f.seek(0x042C);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
+    }
+  }
+
+  void testStripTags1()
+  {
+    ScopedFileCopy copy("empty", ".tta");
+
+    ByteVector audioStream;
+    {
+      TrueAudio::File f(copy.fileName().c_str());
+      f.seek(0x0000);
+      audioStream = f.readBlock(79538);
+
+      CPPUNIT_ASSERT(!f.hasID3v1Tag());
+      CPPUNIT_ASSERT(!f.hasID3v2Tag());
+      f.ID3v2Tag(true)->setTitle("01234 56789 ABCDE FGHIJ");
+      f.save();
+
+      CPPUNIT_ASSERT(!f.hasID3v1Tag());
+      CPPUNIT_ASSERT(f.hasID3v2Tag());
+      f.ID3v1Tag(true)->setTitle("01234 56789 ABCDE FGHIJ");
+      f.save();
+    }
+
+    {
+      TrueAudio::File f(copy.fileName().c_str());
+      CPPUNIT_ASSERT_EQUAL((long)80734, f.length());
+      CPPUNIT_ASSERT(f.hasID3v1Tag());
+      CPPUNIT_ASSERT(f.hasID3v2Tag());
+
+      f.strip(TrueAudio::File::ID3v1);
+      f.save();
+    }
+
+    {
+      TrueAudio::File f(copy.fileName().c_str());
+      CPPUNIT_ASSERT_EQUAL((long)80606, f.length());
+      CPPUNIT_ASSERT(!f.hasID3v1Tag());
+      CPPUNIT_ASSERT(f.hasID3v2Tag());
+
+      f.strip(TrueAudio::File::ID3v2);
+      f.save();
+    }
+
+    {
+      TrueAudio::File f(copy.fileName().c_str());
+      CPPUNIT_ASSERT_EQUAL((long)79538, f.length());
+      CPPUNIT_ASSERT(!f.hasID3v1Tag());
+      CPPUNIT_ASSERT(!f.hasID3v2Tag());
+
+      f.seek(0x0000);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
+    }
+  }
+
+  void testStripTags2()
+  {
+    ScopedFileCopy copy("empty", ".tta");
+
+    ByteVector audioStream;
+    {
+      TrueAudio::File f(copy.fileName().c_str());
+      f.seek(0x0000);
+      audioStream = f.readBlock(79538);
+
+      CPPUNIT_ASSERT(!f.hasID3v1Tag());
+      CPPUNIT_ASSERT(!f.hasID3v2Tag());
+      f.ID3v1Tag(true)->setTitle("01234 56789 ABCDE FGHIJ");
+      f.save();
+
+      CPPUNIT_ASSERT(f.hasID3v1Tag());
+      CPPUNIT_ASSERT(!f.hasID3v2Tag());
+      f.ID3v2Tag(true)->setTitle("01234 56789 ABCDE FGHIJ");
+      f.save();
+    }
+
+    {
+      TrueAudio::File f(copy.fileName().c_str());
+      CPPUNIT_ASSERT_EQUAL((long)80734, f.length());
+      CPPUNIT_ASSERT(f.hasID3v1Tag());
+      CPPUNIT_ASSERT(f.hasID3v2Tag());
+
+      f.strip(TrueAudio::File::ID3v2);
+      f.save();
+    }
+
+    {
+      TrueAudio::File f(copy.fileName().c_str());
+      CPPUNIT_ASSERT_EQUAL((long)79666, f.length());
+      CPPUNIT_ASSERT(f.hasID3v1Tag());
+      CPPUNIT_ASSERT(!f.hasID3v2Tag());
+
+      f.strip(TrueAudio::File::ID3v1);
+      f.save();
+    }
+
+    {
+      TrueAudio::File f(copy.fileName().c_str());
+      CPPUNIT_ASSERT_EQUAL((long)79538, f.length());
+      CPPUNIT_ASSERT(!f.hasID3v1Tag());
+      CPPUNIT_ASSERT(!f.hasID3v2Tag());
+
+      f.seek(0x0000);
+      CPPUNIT_ASSERT_EQUAL(audioStream, f.readBlock(79538));
     }
   }
 
