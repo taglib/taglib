4b12139c69f9033ac5ebfdbcbc506d4808cc07b2
Dāvis <davispuh@gmail.com>
2015-08-01 21:49:20 +0300

    Use volatile for TestFloatFormat

--- cmake/TestFloatFormat.c
+++ cmake/TestFloatFormat.c
@@ -2,16 +2,14 @@ int main(int argc, char **argv)
 {
   int ret = 0;
 
-  double bin1[] = {
+  volatile double bin1[] = {
     // "*TAGLIB*" encoded as a little-endian floating-point number
     (double)3.9865557444897601e-105, (double)0.0
   };
-  float bin2[] = {
+  volatile float bin2[] = {
     // "*TL*" encoded as a little-endian floating-point number
     (float)1.81480400e-013, (float)0.0
   };
-  ret += ((int*)bin1)[argc];
-  ret += ((int*)bin2)[argc];
-
+  ret += (int)(bin1[1]+bin2[1]);
   return ret;
 }
