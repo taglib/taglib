0b2a3ce4002e671355fe8646504051732ac25676
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-11-18 17:27:25 +0900

    Remove const from the key type of APE::ItemListMap.
    A key type must be copy-assignable, and some compilers check it strictly.

--- taglib/ape/apetag.h
+++ taglib/ape/apetag.h
@@ -49,7 +49,7 @@ namespace TagLib {
      *
      * \see APE::Tag::itemListMap()
      */
-    typedef Map<const String, Item> ItemListMap;
+    typedef Map<String, Item> ItemListMap;
 
 
     //! An APE tag implementation
