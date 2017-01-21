a358e87cc43a8ce0cfd13bf040ea72624ae8ed19
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2017-01-21 11:13:49 +0900

    Revert useless changes accidentally committed.

--- taglib/mpeg/mpegutils.h
+++ taglib/mpeg/mpegutils.h
@@ -45,12 +45,12 @@ namespace TagLib
        * \note This does not check the length of the vector, since this is an
        * internal utility function.
        */
-      inline bool isFrameSync(const ByteVector &bytes, long offset = 0)
+      inline bool isFrameSync(const ByteVector &bytes)
       {
         // 0xFF in the second byte is possible in theory, but it's very unlikely.
 
-        const unsigned char b1 = bytes[offset + 0];
-        const unsigned char b2 = bytes[offset + 1];
+        const unsigned char b1 = bytes[0];
+        const unsigned char b2 = bytes[1];
         return (b1 == 0xFF && b2 != 0xFF && (b2 & 0xE0) == 0xE0);
       }
 
