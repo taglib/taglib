e5effdbdd2d8e75e19d564f25e45e842fe15e7f7
Xijian Yan <yanxijian@163.com>
2017-11-06 09:46:12 +0800

    Update mpegfile.cpp
    Code style

--- taglib/mpeg/mpegfile.cpp
+++ taglib/mpeg/mpegfile.cpp
@@ -108,10 +108,8 @@ bool MPEG::File::isSupported(IOStream *stream)
   long headerOffset;
   const ByteVector buffer = Utils::readHeader(stream, bufferSize(), true, &headerOffset);
 
-  if (buffer.isEmpty())
-  {
-	   return false;
-  }
+  if(buffer.isEmpty())
+	  return false;
   
   const long originalPosition = stream->tell();
   AdapterFile file(stream);
