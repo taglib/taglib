036a0317b928f74de63fb197a29dc8e0e1e8cf4f
safu9 <30684866+safu9@users.noreply.github.com>
2018-10-11 01:25:02 +0900

    Add support for file descriptor to FileStream (#832)
    Add support for file descriptor

--- taglib/toolkit/tfilestream.cpp
+++ taglib/toolkit/tfilestream.cpp
@@ -58,6 +58,11 @@ namespace
 #endif
   }
 
+  FileHandle openFile(const int fileDescriptor, bool readOnly)
+  {
+    return InvalidFileHandle;
+  }
+
   void closeFile(FileHandle file)
   {
     CloseHandle(file);
@@ -96,6 +101,11 @@ namespace
   FileHandle openFile(const FileName &path, bool readOnly)
   {
     return fopen(path, readOnly ? "rb" : "rb+");
+  }
+
+  FileHandle openFile(const int fileDescriptor, bool readOnly)
+  {
+    return fdopen(fileDescriptor, readOnly ? "rb" : "rb+");
   }
 
   void closeFile(FileHandle file)
@@ -155,6 +165,25 @@ FileStream::FileStream(FileName fileName, bool openReadOnly)
 # else
     debug("Could not open file " + String(static_cast<const char *>(d->name)));
 # endif
+  }
+}
+
+FileStream::FileStream(int fileDescriptor, bool openReadOnly)
+  : d(new FileStreamPrivate(""))
+{
+  // First try with read / write mode, if that fails, fall back to read only.
+
+  if(!openReadOnly)
+    d->file = openFile(fileDescriptor, false);
+
+  if(d->file != InvalidFileHandle)
+    d->readOnly = false;
+  else
+    d->file = openFile(fileDescriptor, true);
+
+  if(d->file == InvalidFileHandle)
+  {
+    debug("Could not open file using file descriptor");
   }
 }
 
