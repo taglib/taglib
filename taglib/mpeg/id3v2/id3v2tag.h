fced0f46e950c88c4646c081090705e3de6f4d5e
Scott Wheeler <scott@directededge.com>
2019-09-10 13:31:44 +0200

    Add docs for this method

--- taglib/mpeg/id3v2/id3v2tag.h
+++ taglib/mpeg/id3v2/id3v2tag.h
@@ -396,6 +396,9 @@ namespace TagLib {
        */
       void setTextFrame(const ByteVector &id, const String &value);
 
+      /*!
+       * Dowgrade frames from ID3v2.4 (used internally and by default) to ID3v2.3
+       */
       void downgradeFrames(FrameList *existingFrames, FrameList *newFrames) const;
 
     private:
