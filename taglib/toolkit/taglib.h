4c14571647e3391dd8f59473903abc44707b4f1b
Urs Fleisch <ufleisch@users.sourceforge.net>
2020-12-28 11:36:05 +0100

    Update version to 1.12.0

--- taglib/toolkit/taglib.h
+++ taglib/toolkit/taglib.h
@@ -29,8 +29,8 @@
 #include "taglib_config.h"
 
 #define TAGLIB_MAJOR_VERSION 1
-#define TAGLIB_MINOR_VERSION 11
-#define TAGLIB_PATCH_VERSION 1
+#define TAGLIB_MINOR_VERSION 12
+#define TAGLIB_PATCH_VERSION 0
 
 #if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 1)) || defined(__clang__)
 #define TAGLIB_IGNORE_MISSING_DESTRUCTOR _Pragma("GCC diagnostic ignored \"-Wnon-virtual-dtor\"")
