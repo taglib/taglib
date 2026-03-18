a0ed4c585ba139f9fde3be4ee0084213a9faa26a
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-03-18 06:51:14 -0700

    Add BEXT and iXML chunk support to WAV files
    Read, write, and remove Broadcast Audio Extension (BEXT, EBU Tech 3285)
    and iXML metadata chunks in WAV files. BEXT is widely used in broadcast
    and professional audio for originator, description, time reference, and
    loudness metadata. iXML is used by field recorders and DAWs for scene,
    take, and track metadata.
    
    The raw chunk data is exposed as public members on RIFF::WAV::File
    (bextTag as ByteVector, iXMLTag as String) with hasBEXTTag() and
    hasiXMLTag() query methods. Chunks are read during file open and
    written/removed during save() based on whether the members are
    populated or cleared.

--- taglib/riff/wav/wavfile.h
+++ taglib/riff/wav/wavfile.h
@@ -135,6 +135,24 @@ namespace TagLib {
         Info::Tag *InfoTag() const;
 
         /*!
+         * Raw iXML chunk data as a String.
+         * Empty if no iXML chunk is present.
+         * Set this to write/update an iXML chunk on save; clear to remove it.
+         *
+         * \see hasiXMLTag()
+         */
+        String iXMLTag;
+
+        /*!
+         * Raw BEXT (Broadcast Audio Extension) chunk data as a ByteVector.
+         * Empty if no BEXT chunk is present.
+         * Set this to write/update a BEXT chunk on save; clear to remove it.
+         *
+         * \see hasBEXTTag()
+         */
+        ByteVector bextTag;
+
+        /*!
          * This will strip the tags that match the OR-ed together TagTypes from the
          * file.  By default it strips all tags.  It returns \c true if the tags are
          * successfully stripped.
@@ -190,6 +208,20 @@ namespace TagLib {
          * \see InfoTag()
          */
         bool hasInfoTag() const;
+
+        /*!
+         * Returns whether or not the file on disk actually has an iXML chunk.
+         *
+         * \see iXMLTag
+         */
+        bool hasiXMLTag() const;
+
+        /*!
+         * Returns whether or not the file on disk actually has a BEXT chunk.
+         *
+         * \see bextTag
+         */
+        bool hasBEXTTag() const;
 
         /*!
          * Returns whether or not the given \a stream can be opened as a WAV
