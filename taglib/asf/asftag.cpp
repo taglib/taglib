d65d4d6a231df0c4f41ad7ff12dc9e4f654325aa
Stephen Booth <me@sbooth.org>
2026-09-06 09:48:56 -0500

    Avoid use of `toInt`

--- taglib/asf/asftag.cpp
+++ taglib/asf/asftag.cpp
@@ -454,7 +454,7 @@ PropertyMap ASF::Tag::setProperties(const PropertyMap &props)
           addAttribute(name, str.toULongLong());
           break;
         case Attribute::BoolType:
-          addAttribute(name, str.toInt() != 0 || str.upper() == "TRUE");
+          addAttribute(name, !str.isEmpty() && (str == "1" || str.upper() == "TRUE"));
           break;
         case Attribute::GuidType: {
           ByteVector data;
